746d877dafc18d26ac62a1c9bdec039b7069496b
Darrell 803 <803mcavoy@gmail.com>
2020-02-17 11:35:59 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -23,3 +23,13 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
   function accessElementInArray(array, index) {
     return array[index]
   }
+
+  function destructivelyRemoveElementFromBeginningOfArray(array) {
+    array.shift()
+    return array
+  }
+
+  function removeElementFromBeginningOfArray(array) {
+    array.slice(0)
+    return array
+  }
