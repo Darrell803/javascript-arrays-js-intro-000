6270fef9208626c421c14171074d3400eee8ed9e
Darrell 803 <803mcavoy@gmail.com>
2020-02-17 11:15:56 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
   function addElementToBeginningOfArray(array, element) {
-    newArray = [elemnt, ...array]
+    newArray = [element, ...array]
     return newArray
   }
